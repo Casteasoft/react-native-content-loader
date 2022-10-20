dba2879e92c5fb9abc7d687bdb0a83b312679d7a
Cipto Hadi <ciptohadi79@gmail.com>
2022-10-20 16:03:51 +0700

    add more ts types declaration & ensure minification before release

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,22 @@
-/** TODO */
+import React from "react";
+import { StyleProp, ViewProps, ViewStyle } from "react-native";
+
+/** TODO
+ *  already define some ts types, maybe need some recheck
+ */
 declare module "RNContentLoader";
+
+interface WithViewLoaderProps {
+  duration?: number | undefined;
+  bottomtValue?: number | undefined;
+  topValue?: number | undefined;
+  isLoading?: boolean | undefined;
+  numberOfLines?: number | undefined;
+  loaderColorStyle?: StyleProp<ViewStyle> | undefined;
+  style?: StyleProp<ViewStyle> | undefined;
+  children?: React.ReactNode | undefined;
+}
+
+declare function withViewLoader<P extends object>(
+  TargetComponent: React.ComponentType<P>
+): React.FC<P & WithViewLoaderProps & ViewProps>;
